62a468901488f820a5a913e6cda354848ce68dfd
Jennifer Manwell <manwell.fortier@gmail.com>
2022-04-18 13:57:04 -0400

    Added route for shortURL

--- express_server.js
+++ express_server.js
@@ -18,6 +18,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const sURL = req.params.shortURL;
+  const lURL = urlDatabase[sURL];
+  const templateVars = { shortURL: sURL, longURL: lURL };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
